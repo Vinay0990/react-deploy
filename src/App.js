1385e1197c406f4b835e02e665d99b2eaefe7655
Vinaykumar Dubey <vinay.dubey0990@gmail.com>
2020-10-24 19:59:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "./App.css";
+import MaterialTable from "material-table";
 
+const API_SERVICE_URL =
+  "https://books.zoho.com/api/v3/contacts?organization_id=649249007";
+
+const heders = {
+  "Content-Type": "application/json",
+  Authorization: process.env.AUTH_KEY,
+  "Access-Control-Allow-Origin": "*",
+};
 function App() {
+  const initData = { contacts: [] };
+  const [state, setstate] = useState(initData);
+
+  useEffect(() => {
+    const getData = () => {
+      const url = window.location.href;
+      if (url !== "http://localhost:3000/") {
+        Axios.get(API_SERVICE_URL, { headers: heders }).then((data) =>
+          setstate({ ...state, contacts: data.data.contacts })
+        );
+        console.log(state);
+      } else {
+        Axios.get("/contacts")
+          .then((data) => {
+            console.log(data.data.contacts);
+            setstate({ ...state, contacts: data.data.contacts });
+          })
+          .catch((err) => console.log(err));
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MaterialTable
+        title="Contacts List"
+        columns={[
+          { title: "Name", field: "contact_name" },
+          { title: "Company Name", field: "company_name" },
+          { title: "Type", field: "contact_type_formatted" },
+          { title: "Email", field: "email" },
+          { title: "Mobile", field: "mobile" },
+          { title: "Place of Contact", field: "place_of_contact_formatted" },
+          { title: "Created Time", field: "created_time_formatted" },
+          { title: "Status", field: "status" },
+        ]}
+        data={state.contacts}
+        options={{
+          sorting: true,
+          exportButton: true,
+          headerStyle: {
+            backgroundColor: "#01579b",
+            color: "#FFF",
+          },
+
+          rowStyle: {
+            backgroundColor: "aqua",
+          },
+        }}
+      />
     </div>
   );
 }
